105aa93b5e5ea115ee62385f5ccb0eb316a9e4bb
Marcelle Nhemetz <marcelle.frujugar@gmail.com>
2020-04-04 22:41:51 -0300

    Atualizando navbar e adicionando components no App.js

--- gmdv-ex-wbst/src/App.js
+++ gmdv-ex-wbst/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
 import Header from './Header'
-import Main from './Main'
+import Carousel from './Carousel'
+import CardGroup from './CardGroup'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Main />
+        <Carousel />
+        <CardGroup />
+        <Footer />
       </div>
     )
   }
--- gmdv-ex-wbst/src/Header.js
+++ gmdv-ex-wbst/src/Header.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 class Header extends Component {
 	render (){
 		return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<a className="navbar-brand" href="#">Night Witches</a>
 			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
@@ -35,7 +35,7 @@ class Header extends Component {
 					</ul>
 					<form className="form-inline my-2 my-lg-0">
 							<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-							<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+							<button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
 					</form>
 			</div>
 	</nav>
